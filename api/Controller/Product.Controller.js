5ceb8c375af5ed4cbf926572ca848c1d91d44b83
ibrahimfadily <ibrahem.fdela67@gmail.com>
2023-08-30 19:07:44 +0300

    .

--- api/Controller/Product.Controller.js
+++ api/Controller/Product.Controller.js
@@ -1,46 +1,46 @@
-const productModule = require("../modules/product.module");
+// const productModule = require("../modules/product.module");
 
-const createProduct = (req, res) => {
-  const { name, categories, wight } = req.body;
-  // const name = req.body.name
-  // categories = req.body.categories
-  // wight = req.body.wight
+// const createProduct = (req, res) => {
+//   const { name, categories, wight } = req.body;
+//   // const name = req.body.name
+//   // categories = req.body.categories
+//   // wight = req.body.wight
 
-  productModule
-    .create({
-      name,
-      // name: name
-      categories,
-      wight,
-    })
-    .then((p) => {
-      res.status(200).json({ message: "Done", product: p });
-    });
-};
+//   productModule
+//     .create({
+//       name,
+//       // name: name
+//       categories,
+//       wight,
+//     })
+//     .then((p) => {
+//       res.status(200).json({ message: "Done", product: p });
+//     });
+// };
 
-const getProductCategories = async (req, res) => {
-  const allProducts = await productModule.find();
+// const getProductCategories = async (req, res) => {
+//   const allProducts = await productModule.find();
 
-  var categories = [];
-  allProducts.forEach((product) => {
-    categories.push(...product.categories);
-  });
+//   var categories = [];
+//   allProducts.forEach((product) => {
+//     categories.push(...product.categories);
+//   });
 
-  const categirySet = new Set(categories);
-  categories = Array.from(categirySet);
+//   const categirySet = new Set(categories);
+//   categories = Array.from(categirySet);
 
-  res.status(200).json(categories);
-};
+//   res.status(200).json(categories);
+// };
 
-const deleteProducte = (req , res) => {
-    productModule.deleteOne({name:req.body.name})
-    .then(delRes => {
-        res.status(200).json(delRes)
-    })
-}
+// const deleteProducte = (req , res) => {
+//     productModule.deleteOne({name:req.body.name})
+//     .then(delRes => {
+//         res.status(200).json(delRes)
+//     })
+// }
 
-module.exports = {
-  createProduct,
-  getProductCategories,
-  deleteProducte
-};
+// module.exports = {
+//   createProduct,
+//   getProductCategories,
+//   deleteProducte
+// };
--- api/routs/Produst.route.js
+++ api/routs/Produst.route.js
@@ -1,10 +1,10 @@
-const { Router } = require("express");
-const { createProduct, getProductCategories, deleteProducte } = require("../Controller/Product.Controller");
+// const { Router } = require("express");
+// const { createProduct, getProductCategories, deleteProducte } = require("../Controller/Product.Controller");
 
-const productRouter = Router() ; 
+// const productRouter = Router() ; 
 
-productRouter.post('/createProduct' , createProduct )
-productRouter.get('/getProductCategories' , getProductCategories )
-productRouter.delete('/deleteProducte' , deleteProducte )
+// productRouter.post('/createProduct' , createProduct )
+// productRouter.get('/getProductCategories' , getProductCategories )
+// productRouter.delete('/deleteProducte' , deleteProducte )
 
-module.exports = productRouter
+// module.exports = productRouter
