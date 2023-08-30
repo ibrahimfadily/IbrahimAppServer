0e2a2f43da464940b0937a73675010460a53fca0
ibrahimfadily <ibrahem.fdela67@gmail.com>
2023-08-30 19:33:24 +0300

    ..

--- api/Controller/Product.Controller.js
+++ api/Controller/Product.Controller.js
@@ -1,10 +1,14 @@
+const userModule = require("../modules/user.module");
 const productModule = require("../modules/product.module");
+
 
 const createProduct = (req, res) => {
   const { name, categories, wight } = req.body;
   // const name = req.body.name
   // categories = req.body.categories
   // wight = req.body.wight
+
+
 
   productModule
     .create({
