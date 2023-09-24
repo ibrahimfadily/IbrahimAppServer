2bd23e42676316e099c64b34f5b7fc7ddf768336
ibrahimfadily <ibrahem.fdela67@gmail.com>
2023-09-24 22:51:21 +0300

    Update App.js

--- App.js
+++ App.js
@@ -16,7 +16,36 @@ mongoose.connection.on("connected", () =>  {
 console.log("mongo connected");
 });
 
+import { UserRegistration } from "./UserRegistration";
+/*
+Your functions here
+*/
+return (
+  <>
+    <StatusBar />
+    <SafeAreaView style={styles.container}>
+      <>
+        <Text style={styles.title}>React Native on Back4App</Text>
+        <Text>User registration tutorial</Text>
+        <UserRegistration />
+      </>
+    </SafeAreaView>
+  </>
+);
 
+// Remember to add some styles at the end of your file
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 // // http://localhost:7000/app
 // app.get("/app",(req,res)=>{
 //     res.status(200).json({
