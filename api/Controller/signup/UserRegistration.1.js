fa02d22331680ac27927ea40a63570cb4f32815b
ibrahimfadily <ibrahem.fdela67@gmail.com>
2023-09-24 23:12:54 +0300

    .

--- api/Controller/signup/UserRegistration.1.js
+++ api/Controller/signup/UserRegistration.1.js
@@ -1,10 +1,31 @@
 import React, { FC, ReactElement, useState } from "react";
-import { Button, StyleSheet, TextInput } from "react-native";
+import { Alert, Button, StyleSheet, TextInput } from "react-native";
 import Parse from "parse/react-native";
 
 export const UserRegistration = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const doUserRegistration = async function () {
+    // Note that these values come from state variables that we've declared before
+    const usernameValue = username;
+    const passwordValue = password;
+    // Since the signUp method returns a Promise, we need to call it using await
+    return await Parse.User.signUp(usernameValue, passwordValue)
+      .then((createdUser) => {
+        // Parse.User.signUp returns the already created ParseUser object if successful
+        Alert.alert(
+          "Success!",
+          `User ${createdUser.get("username")} was successfully created!`
+        );
+        return true;
+      })
+      .catch((error) => {
+        // signUp can fail if any parameter is blank or failed an uniqueness check on the server
+        Alert.alert("Error!", error.message);
+        return false;
+      });
+  };
 
   return (
     <>
@@ -22,7 +43,7 @@ export const UserRegistration = () => {
         secureTextEntry
         onChangeText={(text) => setPassword(text)}
       />
-      <Button title={"Sign Up"} onPress={() => {}} />
+      <Button title={"Sign Up"} onPress={() => doUserRegistration()} />
     </>
   );
 };
@@ -31,6 +52,6 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     marginBottom: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
