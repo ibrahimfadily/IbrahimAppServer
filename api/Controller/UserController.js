ac2e592aa584ac5d49d161c4dd2eabbc0ed72303
ibrahimfadily <ibrahem.fdela67@gmail.com>
2023-09-27 19:08:08 +0300

    Update UserController.js

--- api/Controller/UserController.js
+++ api/Controller/UserController.js
@@ -41,37 +41,38 @@ app.post('/register', async (req, res) => {
 
 
 
-// Register: async (req, res) => {
-//   try {
-//     // Get user input
-//     const { email, password } = req.body;
+Register: async (req, res) => {
+  try {
+    // Get user input
+    const { email, password } = req.body;
   
-//     // Validate user input
-//     if (!(email && password)) {
-//       res.status(404).json({ message: "All input is required" });
-//     }
-//     // check if user already exist
-//     // Validate if user exist in our database
-//     const oldUser = await userModule.findOne({ email });
+    // Validate user input
+    if (!(email && password)) {
+      res.status(404).json({ message: "All input is required" });
+    }
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await userModule.findOne({ email });
 
-//     if (oldUser) {
-//       return res
-//         .status(200)
-//         .json({ message: "User Already Exist. Please Login" });
-//     }
-//     //Encrypt user password
-//     encryptedPassword = await bcrypt.hash(password, 10);
+    if (oldUser) {
+      return res
+        .status(200)
+        .json({ message: "User Already Exist. Please Login" });
+    }
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
 
-//     // Create user in our database
-//     const user = await userModule.create({
-//       email: email.toLowerCase(), // sanitize: convert email to lowercase
-//       password: encryptedPassword,
-//     });
+    // Create user in our database
+    const user = await userModule.create({
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
   
 module.exports = {
   Login,
   Register,
 }
-// }
-// }
-// }
+}
+}
+
+
