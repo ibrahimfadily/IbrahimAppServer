9acd4656097aedcd3bd81fb99b2d07a392ab38c2
ibrahimfadily <ibrahem.fdela67@gmail.com>
2024-02-10 23:42:11 +0200

    v

--- api/modules/user.module.js
+++ api/modules/user.module.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-
+// Define the schema for the User model
 const UserSchema = new mongoose.Schema({
-    pass: { type: String, required: true },
-    username: { type: String, unique: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, unique: true },
-
-    data: {}
+    pass: { type: String, required: true }, // User password
+    username: { type: String, unique: true }, // User username (must be unique)
+    email: { type: String, required: true, unique: true }, // User email (required and unique)
+    phone: { type: String, unique: true }, // User phone number (unique)
+    data: {} // Additional user data can be stored here
 });
 
-const userModule = mongoose.model('User', UserSchema);
-
-module.exports = userModule
+// Create and export the User model based on the schema
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
