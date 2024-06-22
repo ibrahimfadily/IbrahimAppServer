a1f312a66c3cfe5908f24ad1f5216e8801f783f3
ibrahimfadily <ibrahem.fdela67@gmail.com>
2024-06-22 15:10:11 +0300

    Update user.module.js

--- api/modules/user.module.js
+++ api/modules/user.module.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     pass: { type: String, required: true }, // User password
     username: { type: String, unique: true }, // User username (must be unique)
     email: { type: String, required: true, unique: true }, // User email (required and unique)
-    data: {} // Additional user data can be stored here
+    data: {} , // Additional user data can be stored here
+    coins :{type: Number , default: 0}
 });
 
 // Create and export the User model based on the schema
